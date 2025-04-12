9781318b37328e10ea9985fbb259dbe720b608f1
jaydeep <lakhanijaydeep9460@gmail.com>
2025-04-12 19:28:48 +0530

    feat: added new pages

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ AOS.init({
   // disable: function () {
   // return window.innerWidth < 767;
   // },
+  easing: "ease-in-out",
 });
 
 // ************************************************************************
@@ -39,21 +40,47 @@ document.addEventListener("DOMContentLoaded", function () {
   let hero = document.querySelector(".hero");
   let heroContent = document.querySelector(".hero__content");
 
-  // Fade-in Effect on Load
-  setTimeout(() => {
-    heroContent.style.opacity = "1";
-    heroContent.style.transform = "translateY(0)";
-  }, 500);
+  if (hero && heroContent) {
+    // Fade-in Effect on Load
+    setTimeout(() => {
+      heroContent.style.opacity = "1";
+      heroContent.style.transform = "translateY(0)";
+    }, 500);
 
-  // Parallax Scroll Effect
-  window.addEventListener("scroll", function () {
-    let scrollTop = window.scrollY;
+    // Parallax Scroll Effect
+    window.addEventListener("scroll", function () {
+      let scrollTop = window.scrollY;
 
-    // Background moves at a slower pace than scroll for parallax effect
-    hero.style.backgroundPosition = `center ${scrollTop * 0.3}px`;
+      // Background moves at a slower pace than scroll for parallax effect
+      hero.style.backgroundPosition = `center ${scrollTop * 0.3}px`;
 
-    // Opacity & Scale Effect
-    let opacity = 1 - scrollTop * 0.001;
-    hero.style.opacity = opacity > 0.3 ? opacity : 0.3;
+      // Opacity & Scale Effect
+      let opacity = 1 - scrollTop * 0.001;
+      hero.style.opacity = opacity > 0.3 ? opacity : 0.3;
+    });
+  }
+});
+
+// ************************************************************************
+// START : intro__link scroll
+// ************************************************************************
+
+document.querySelectorAll(".intro__link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetEl = document.getElementById(targetId);
+    const headerEl = document.querySelector(".header");
+    const headerHeight = headerEl.offsetHeight;
+
+    const elementPosition =
+      targetEl.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - headerHeight - 20;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
   });
 });
